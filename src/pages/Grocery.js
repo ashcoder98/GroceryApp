e4604319658c746f3acd2e52ef64fe5486c4b73c
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2022-01-03 00:22:41 -0500

    adds some logic to grocery tracker

--- src/pages/Grocery.js
+++ src/pages/Grocery.js
@@ -1,27 +1,39 @@
 import '../App.css';
+import {useState , useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlusCircle, faChevronRight, faChevronLeft, faCircle, faCheckCircle, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 function Grocery() {
+
+        const [items, setItems] = useState 
+        ([
+          { itemName: "item 1", quantity: 1, isSelected: false },
+          { itemName: "item 2", quantity: 3, isSelected: true },
+          { itemName: "item 3", quantity: 2, isSelected: false },
+      ]);
+
+
+
+
   return (
     <div className="Grocery">
       <div className='maincontainer'>
-      <h1>Grocery Tracker</h1>
+      <h1 className='trackertitle'>Grocery Tracker</h1>
       <div className='add-item-box'>
         <input className='add-item-input' placeholder='Add new item...' />
       <FontAwesomeIcon icon={faPlusCircle} ></FontAwesomeIcon>
         </div>
         <div className='item-list'>
-					<div className='item-container'>
+          {items.map((item, index)=>					<div className='item-container'>
 						<div className='item-name'>
-							{false ? (
+							{item.isSelected ? (
 								<>
 									<FontAwesomeIcon icon={faCheckCircle} />
-									<span className='completed'>Item 1</span>
+									<span className='completed'>{item.itemName}</span>
 								</>
 							) : (
 								<>
 									<FontAwesomeIcon icon={faCircle} />
-									<span>Item 1</span>
+									<span>{item.itemName}</span>
 								</>
 							)}
 						</div>
@@ -34,12 +46,13 @@ function Grocery() {
 								<FontAwesomeIcon icon={faChevronRight} />
 							</button>
 						</div>
-					</div>
-				</div>
+					</div>)}
+
 				<div className='total'>
         <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
-          Total: 6
+          <span className='totalnum'>Total: 6</span>
         </div>
+				</div>
 
 
 
