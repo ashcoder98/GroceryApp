f6bdcca1fb29e55dc44d9c31b4eb76c23a0eade7
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2022-01-17 00:43:33 -0500

    adding api

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,12 +1,27 @@
 import '../App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHammer, faHardHat } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Recipe() {
+const [ recipe, setRecipe ] = useState(null)
+  const url = `s=Arrabiata`
+  
+  useEffect (() => {axios.get(url)
+    .then(response => {
+      const recipes = response.data
+      setRecipe(recipes)
+      console.log(recipes)
+    });
+
+  }, []);
+  
+  
   return (
     <div className="Recipe">
       <h1><FontAwesomeIcon icon={faHardHat}></FontAwesomeIcon> Work in progress <FontAwesomeIcon icon={faHammer}></FontAwesomeIcon></h1>
-      
+      <h1>{meals.strIngredient1}</h1>
     </div>
   );
 }
